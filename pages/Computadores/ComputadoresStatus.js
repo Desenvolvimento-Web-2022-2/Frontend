e45ec68aec688b35a561a9019f37a4340f5ee95f
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2022-10-10 17:31:53 -0300

    Status Computadores html/css implementado

---
+++ pages/Computadores/ComputadoresStatus.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    var sidebar = document.getElementsByClassName("sidebarName").item(0)
+
+    var AtualizarSala = document.createElement("custom-button")
+    AtualizarSala.setAttribute("redirect","/")
+    AtualizarSala.setAttribute("labelName","Atualizar Sala")
+    AtualizarSala.classList.add("save-button")
+    AtualizarSala.classList.add("color-white")
+
+    var RemoverSala = document.createElement("custom-button")
+    RemoverSala.setAttribute("redirect","/")
+    RemoverSala.setAttribute("labelName","Remover Sala")
+    RemoverSala.classList.add("save-button")
+    RemoverSala.classList.add("color-red")
+
+    var ReservarSala = document.createElement("custom-button")
+    ReservarSala.setAttribute("redirect","/")
+    ReservarSala.setAttribute("labelName","Reservar Sala")
+    ReservarSala.classList.add("save-button")
+    ReservarSala.classList.add("color-green")
+   
+    sidebar.appendChild(AtualizarSala)
+    sidebar.appendChild(RemoverSala)
+    sidebar.appendChild(ReservarSala)
+}

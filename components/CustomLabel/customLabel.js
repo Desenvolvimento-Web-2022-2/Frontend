9e22ec37039e12989ba3515bc922ce288e376abe
unknown <victorehrich@gmail.com>
2022-10-10 17:39:24 -0300

    removing comments

--- components/CustomLabel/customLabel.js
+++ components/CustomLabel/customLabel.js
@@ -38,12 +38,6 @@ customElements.define("custom-label-user", CustomLabelUser)
 function showColapse(button,id,labelType){
     button.classList.toggle('arrowInvert')
     var content = document.getElementsByClassName("buttonDiv")[id-1]
-    // if(content.classList.contains("collapsed")){
-    //     content.style.display = "none"
-    // }else{
-    //     content.style.display = "grid"
-    // }
-    // content.classList.toggle("collapsed")
     if(labelType = "user"){
         if(content.classList.contains("collapsed")){
             content.innerHTML = ""
@@ -67,28 +61,31 @@ class CustomLabelComputer extends HTMLElement{
     connectedCallback() {
         const imgPrefix = "../../imgs/"
         var usersInfos = {
-            name: this.hasAttribute("usersInfosName")? this.getAttribute("usersInfosName"): "",
-            email: this.hasAttribute("usersInfosEmail")? this.getAttribute("usersInfosEmail"): "",
-            role: this.hasAttribute("usersInfosRole")? this.getAttribute("usersInfosRole"): ""
+            name: this.hasAttribute("usersInfosName")? this.getAttribute("usersInfosName"): "a",
+            email: this.hasAttribute("usersInfosEmail")? this.getAttribute("usersInfosEmail"): "b",
+            role: this.hasAttribute("usersInfosRole")? this.getAttribute("usersInfosRole"): "c"
         }
+        let id = this.hasAttribute("id")? this.getAttribute("id"): ""
         this.innerHTML=`
-        <div id="supergeneral">
-            <div id="general">
-                <div class="son">
-                    <p>Nome</p>
-                    <p>Email</p>
-                    <p>Cargo</p>
+            <div class="mainContainer">
+                <div style="display:grid;grid-template-columns:90% 10%;    align-items: center;">
+                    <div class="itensDiv">
+                        <p>Nome</p>
+                        <p>Email</p>
+                        <p>Cargo</p>
+                        <p>${usersInfos.name}</p>
+                        <p>${usersInfos.email}</p>
+                        <p>${usersInfos.role}</p>
+                    </div>
+                    <div class="botao">
+                        <button onclick="showColapse(this,${id},'computer')" id="${id}"><img src="${imgPrefix}icons/ArrowDown.svg" id="arrow" class="arrow"></button>
+                    </div>
                 </div>
-                <div class="son">
-                    <p>${usersInfos.name}</p>
-                    <p>${usersInfos.email}</p>
-                    <p>${usersInfos.role}</p>
+                <div class="buttonDiv" id="${id}">
+                    
                 </div>
+                
             </div>
-            <div class="botao">
-                <button><img src="${imgPrefix}icons/ArrowDown.svg"></button>
-            </div>
-        </div>
         `
     }
 }

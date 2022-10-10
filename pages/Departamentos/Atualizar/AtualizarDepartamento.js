d2ac6ce6b5eb3716396c4199a35a12a0395c5564
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2022-10-10 15:43:25 -0300

    Implementado o css/hmtl de atualizar departamento

---
+++ pages/Departamentos/Atualizar/AtualizarDepartamento.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    var sidebar = document.getElementsByClassName("sidebarName").item(0)
+
+    var SalvarAlteracaoDept = document.createElement("custom-button")
+    SalvarAlteracaoDept.setAttribute("redirect","/")
+    SalvarAlteracaoDept.setAttribute("labelName","Salvar")
+    SalvarAlteracaoDept.classList.add("save-button")
+    SalvarAlteracaoDept.classList.add("color-white")
+
+    sidebar.appendChild(SalvarAlteracaoDept)
+
+}

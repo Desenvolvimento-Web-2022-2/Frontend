e23ba717f0d2e01cda4ed1698596358edb5d87c8
victor medeiros <victorehrich@gmail.com>
2022-12-03 21:13:51 -0300

    arquivos que faltaram da api

--- api/Server/Controllers/busca.js
+++ api/Server/Controllers/busca.js
@@ -3,29 +3,37 @@ const buscaService = require("../Services/busca")
 
 class BuscaController{
     async getSearch(req, res){
-        let request = JSON.parse(req.body.req)
-        console.log(request)
-        let json
-        switch(request.page){
-            case "Blocos":
-                json = buscaService.getBlocos(request)
-                res.send(json)
-                break
-            case "Computadores":
-                json = buscaService.getComputadores(request)
-                break
-            case "Users":
-                json = buscaService.getUsers(request)
-                break
-            case "Salas":
-                json = buscaService.getSalas(request)
-                break
-            default:
-                res.status(404)
-                res.send("Not Found")
-                break
-        } 
-        res.json(json)
+        try{
+            let request = JSON.parse(req.body.req)
+            let json
+            switch(request.page){
+                case "Blocos":
+                    json = buscaService.getBlocos(request)
+                    res.send(json)
+                    break
+                case "Computadores":
+                    json = buscaService.getComputadores(request)
+                    res.send(json)
+                    break
+                case "Users":
+                    json = buscaService.getUsers(request)
+                    res.send(json)
+                    break
+                case "Salas":
+                    json = buscaService.getSalas(request)
+                    res.send(json)
+                    break
+                default:
+                    res.status(404)
+                    res.send("Not Found")
+                    break
+            } 
+        }catch(err){
+            console.log(err)
+            res.status(500)
+            res.send("Internal Server Error")
+        }
+        
     }
 }
 module.exports = new BuscaController()
--- api/Server/Services/busca.js
+++ api/Server/Services/busca.js
@@ -1,10 +1,57 @@
 const blocos = require("../db/Blocos.json")
-
+const salas = require("../db/Salas.json")
+const computadores = require("../db/Computadores.json")
+const users = require("../db/Users.json")
+const profile = require("../db/Profiles.json")
 class BuscaService{
     getComputadores(body){
+        let computerArray = []
+        if(body.input == ""){
+            computadores.Computadores.forEach(computador=>{
+                if(computador.salaId == body.ids.salaId)
+                    computerArray.push(computador)
+            })
+        }
+        else{
+            computadores.Computadores.forEach(computador=>{
+                if(
+                    computador[`${body.select}`].toString().toLowerCase().includes(body.input.toLowerCase()) &&
+                    computador.salaId == body.ids.salaId){
+                        computerArray.push(computador)
+                }
+            })
+        }
+        return computerArray
     }
     getUsers(body){
-        
+        let usersArray = []
+        if(body.input == ""){
+            users.Users.forEach(user=>{
+                delete user.usersInfosPassword
+                profile.Profiles.forEach(p=>{
+                    if(p.id == user.profileId){
+                        delete user.profileId
+                        user.profile = p
+                    }
+                })
+                usersArray.push(user)
+            })
+        }
+        else{
+            users.Users.forEach(user=>{
+                if(user[`${body.select}`].toString().toLowerCase().includes(body.input.toLowerCase())){
+                    delete user.usersInfosPassword
+                    profile.Profiles.forEach(p=>{
+                        if(p.id == user.profileId){
+                            delete user.profileId
+                            user.profile = p
+                        }
+                    })
+                    usersArray.push(user)
+                }
+            })
+        }
+        return usersArray
     }
     getBlocos(body){
         let blocosArray = []
@@ -15,7 +62,7 @@ class BuscaService{
         }
         else{
             blocos.Blocos.forEach(bloco=>{
-                if(bloco[`${body.select}`].toString().toLowerCase() == body.input.toLowerCase()){
+                if(bloco[`${body.select}`].toString().toLowerCase().includes(body.input.toLowerCase())){
                     blocosArray.push(bloco)
                 }
             })
@@ -23,7 +70,23 @@ class BuscaService{
         return blocosArray
     }
     getSalas(body){
-        
+        let salasArray = []
+        if(body.input == ""){
+            salas.Salas.forEach(sala=>{
+                if( sala.blocoId == body.ids.blocoId)
+                    salasArray.push(sala)
+            })
+        }
+        else{
+            salas.Salas.forEach(sala=>{
+                if(
+                    sala[`${body.select}`].toString().toLowerCase().includes(body.input.toLowerCase()) && 
+                    sala.blocoId == body.ids.blocoId){
+                    salasArray.push(sala)
+                }
+            })
+        }
+        return salasArray
     }
 }
 module.exports = new BuscaService()
